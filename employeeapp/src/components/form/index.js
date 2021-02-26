499757336d60f7adda0f78df18002679f661684f
Shaun Limbeek <shaun.limbeek@gmail.com>
2021-02-25 23:01:34 -0800

    created the html in the return for the form

--- employeeapp/src/components/form/index.js
+++ employeeapp/src/components/form/index.js
@@ -1,12 +1,34 @@
 import { useRef } from "react";
-import React {useRef} from "react";
+import React, { useRef } from "react";
+import { useEmployeeContext } from "../../utils/GlobalState";
 
-function Form(props) {
-  return <form className="list-group-item bg-light text-secondary">
-      
-      {props.item}
-      
-      </form>;
+function Form() {
+  const nameRef = useRef();
+  const imgRef = useRef();
+  const phoneRef = useRef();
+  const emailRef = useRef();
+  const dobRef = useRef();
+  const [_, dispatch] = useEmployeeContext();
+
+  function handleSubmit(e) {
+
+  }
+
+  return (
+   <div>
+      <h4>Add an Employee:</h1>
+      <form className="form-group mt-5" onSubmit={handleSubmit}>
+        <input className="form-control" ref={nameRef} name="name" placeholder="Enter name" />
+        <input className="form-control" ref={imgRef} name="img" placeholder="Enter img url" />
+        <input className="form-control" ref={phoneRef} name="phone" placeholder="Enter phone number" />
+        <input className="form-control" ref={emailRef} name="email" placeholder="Enter email address" />
+        <input className="form-control" ref={dobRef} name="dob" placeholder="Enter employee date of birth" />
+        <button className="btn btn-success mt-3 mb-5" type="submit">
+          Add to Employee Contact List
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default Form;
